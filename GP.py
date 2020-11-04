bc0869dedf86aa32da1730013f9c8c405c14f986
Jawahar <Jtunuguntla@gmail.com>
2020-11-04 13:31:26 +0000

    commit initial project

---
+++ GP.py
@@ -0,0 +1,11 @@
+class GP:
+    """GP Class and attributes."""
+
+    def add_availability(self):
+        pass
+
+    def confirm_appointment(self):
+        pass
+
+    def input_prescription(self):
+        pass
