07d6720149cd6c1132b7c6d606133fcc91fab746
Donald954732 <lpchungaa@gmail.com>
2020-12-05 16:16:24 +0000

    Finished DB added encrypting and password function

--- GPDB.sql
+++ GPDB.sql
@@ -5,17 +5,22 @@ CREATE TABLE UserGroup (
   UserType varchar(10) PRIMARY KEY
 );
 
+INSERT INTO UserGroup VALUES ('GP');
+INSERT INTO UserGroup VALUES ('Admin');
+INSERT INTO UserGroup VALUES ('Patient');
+
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
   ID varchar(10) PRIMARY KEY CHECK ((ID LIKE 'G%') OR (ID LIKE 'A%') OR (ID BETWEEN 1000000000 AND 9999999999)),
   username varchar(50) UNIQUE,
-  passCode varchar(40),
-  firstName varchar(50),
-  lastName varchar(50),
-  phoneNo varchar(20) UNIQUE,
-  HomeAddress varchar(100),
-  postCode varchar(9),
+  passCode varchar(64),
+  firstName BLOB,
+  lastName BLOB,
+  phoneNo BLOB,
+  HomeAddress BLOB,
+  postCode BLOB,
   UserType varchar(10),
+  Deactivated char(1) CHECK(Deactivated IN ('T','F')),
   FOREIGN KEY (UserType) REFERENCES UserGroup (UserType) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -33,10 +38,10 @@ CREATE TABLE VisitBooking (
   StaffID integer(10),
   Timeslot datetime,
   Rating int CHECK(Rating >= 0 OR Rating <= 10),
-  Confirmed char(1) CHECK(Confirmed IN ('Y', 'N')),
-  Attended char(1) CHECK(Attended IN ('Y', 'N')),
-  Diagnosis text,
-  Notes text,
+  Confirmed char(1) CHECK(Confirmed IN ('T', 'F')),
+  Attended char(1) CHECK(Attended IN ('T', 'F', 'P')),
+  Diagnosis BLOB,
+  Notes BLOB,
   PRIMARY KEY (NHSNo, StaffID, Timeslot),
   FOREIGN KEY (StaffID, Timeslot) REFERENCES available_time (StaffID, Timeslot) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (NHSNo) REFERENCES Users (ID) ON DELETE CASCADE ON UPDATE CASCADE
@@ -47,9 +52,9 @@ CREATE TABLE perscription (
   NHSNo integer(10),
   StaffID integer(10),
   Timeslot datetime,
-  drugName varchar(70),
-  quantity integer,
-  drugNotes text,
+  drugName BLOB,
+  quantity BLOB,
+  Instructions BLOB,
   PRIMARY KEY (NHSNo, StaffID, Timeslot, drugName),
   FOREIGN KEY (NHSNo, StaffID, Timeslot) REFERENCES VisitBooking (NHSNo, StaffID, Timeslot) ON DELETE CASCADE ON UPDATE CASCADE
 );
