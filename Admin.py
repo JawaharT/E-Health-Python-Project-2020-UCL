de87eb89f3a42fdceba3cd7f8566c33529e7a2c1
ucabxq0 <ucabxq0@ucl.ac.uk>
2020-12-14 17:04:26 +0000

    update

--- Admin.py
+++ Admin.py
@@ -1,4 +1,13 @@
-class Admin:
+import os
+from tabulate import tabulate
+from parserHelp import parser
+from login import currentUser
+from databaseHelp import SQLQuerry
+import time
+import sys
+import datetime
+
+class AdminNavigator():
     """Admin class and attributes."""
 
     def add_GP(self):
