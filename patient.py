9e8db8fd50e8f101cd96398892770872bbe65523
Wang-Jade-729 <zczlaae@ucl.ac.uk>
2020-12-27 20:30:10 +0000

    choose data - show available GP - choose GP - choose time - check repeat befor insert into visit - delete from available_time bookingNo is a random number ###### checked repeat

--- patient.py
+++ patient.py
@@ -1,51 +1,183 @@
-import os
 from tabulate import tabulate
 from main import User, MenuHelper
 
-from encryption import EncryptionHelper, PasswordHelper
+import random
+from encryption import encryptionHelper
+from parser_help import Parser
 from database_help import SQLQuery
 import time
-import sys
+# import sys
+import datetime
+from main import User
 
-from parser_help import Parser
+print_clean = Parser.print_clean
+delta = datetime.timedelta
 
 
 class Patient(User):
-    """Navigate through Patient features."""
+    """
+    GP Class with navigation options and various functionalities.
+    """
 
-    def PatientNavigator(self):
+    def main_menu(self) -> None:
         """
-        Main Menu for Patient-type users.
+        Main Menu for GP-type users.
         """
         while True:
-            user_input = Parser.selectionParser(
-                options={"A": "Book Appointments", "B": "Cancel Appointments", "C": "Edit Appointments",
-                         "D": "Review Appointments", "--logout": "logout"})
-            if user_input == "--logout":
+            print("You're currently viewing main menu options for Patient {}.".format(self.username))
+            option_selection = Parser.selection_parser(
+                options={"B": "book appointments", "C": "cancel appiontment", "R": "review appointments",
+                         "--logout": "Logout"})
+            if option_selection == "--logout":
+                # Quitting is required for logout to ensure all personal data is cleared from session
+                print_clean("Logging you out...")
                 Parser.user_quit()
+            elif option_selection == "B":
+                self.book_appointment()
+            elif option_selection == "C":
+                self.cancel_appointment()
+            elif option_selection == "R":
+                self.review_appointment()
 
-            if user_input == "A":
-                self.bookApmt()
-            elif user_input == "B":
-                self.cancelApmt()
-            elif user_input == "C":
-                self.editApmt()
-            else:
-                self.reviewApmt()
 
-    @staticmethod
-    def reviewApmt():
+    def book_appointment(self):
+        """
+
+        """
+        stage = 0
+        while True:
+            while stage == 0:
+                # choose a date
+                selected_date = Parser.date_parser(question=f"Managing for Patient {self.username}.\n"
+                                                            "Select a Date:\n")
+
+                if selected_date == "--back":
+                    print_clean()
+                    return
+
+                # show available GP
+                gp_result = SQLQuery(
+                    "SELECT DISTINCT StaffID FROM available_time "
+                    "WHERE Timeslot >= ? AND Timeslot <= ?",
+                ).executeFetchAll(parameters=(selected_date, selected_date + delta(days=1)))
+                gp_table = []
+                for i in range(len(gp_result)):
+                    gp_table.append([i + 1, str(gp_result[i][0])])
+                print(f"You are viewing all available GP for: {selected_date}")
+                if len(gp_table) == 0:
+                    print(f"there is no booking for this day yet.")
+                    stage = 0
+                    input("Press Enter to continue.")
+                else:
+                    print(tabulate(gp_table, headers=["Pointer", "GP"]))
+                    stage = 1
+
+            while stage == 1:
+                # show list of time
+                selected_gp = Parser.gp_no_parser(question=f"Managing for Patient {self.username}.\n"
+                                                           "Select a GP:")
+                appointment_result = SQLQuery(
+                    "SELECT StaffID, Timeslot FROM available_time "
+                    "WHERE StaffID = ? AND Timeslot >= ? AND Timeslot <= ?",
+                ).executeFetchAll(parameters=(selected_gp, selected_date, selected_date + delta(days=1)))
+
+                appointment_table_raw = []
+                appointment_table = []
+
+                for i in range(len(appointment_result)):
+                    appointment_table.append([i + 1, str(appointment_result[i][0]), str(appointment_result[i][1])])
+                    appointment_table_raw.append([i + 1, appointment_result[i][0], appointment_result[i][1]])
+                print(f"You are viewing your schedule for: {selected_date}")
+                if len(appointment_table) == 0:
+                    print(f"there is no booking for this day yet.")
+                    stage = 0
+                else:
+                    print(tabulate(appointment_table, headers=["Pointer", "GP", "Timeslot"]))
+
+                    stage = 2
+
+            while stage == 2:
+                selected_appointment = Parser.integer_parser(question="Select entry using number from Pointer column or "
+                                                            "type '--back' to go back")
+                if selected_appointment == '--back':
+                    return False
+
+                selected_row = appointment_table[selected_appointment - 1]
+                selected_row_raw = appointment_table_raw[selected_appointment - 1]
+
+                print("This is time slot will be booked by you:")
+
+                print(tabulate([selected_row], headers=["Pointer", "GP", "Timeslot"]))
+
+                confirm = Parser.selection_parser(options={"Y": "Confirm", "N": "Go back and select again"})
+                # Confirm if user wants to delete slots
+                if confirm == "Y":
+                    try:
+                        repeat_booking_num = 1
+                        while (repeat_booking_num ):
+                            bookingNum = random.randint(100000, 999999)
+                            #bookingNum = random.randint(1, 3)
+                            #bookingNum = 1
+                            visit_same_booking_num = SQLQuery("SELECT bookingNo FROM visit WHERE bookingNo = ? ").executeFetchAll(parameters=(bookingNum,))
+                            if len(visit_same_booking_num) == 0:
+                                #repeat_booking_num = 0
+                                break
+                            else:
+                                repeat_booking_num  = 1
+
+                        visit_same_booking_num = SQLQuery(
+                            "SELECT Timeslot FROM visit WHERE Timeslot = ? ").executeFetchAll(parameters=(selected_row_raw[2],))
+                        if len(visit_same_booking_num) != 0:
+                            # repeat_timeslot_num = 0
+                            print("This time slot has been booked")
+                            return False
+                        else:
+
+                            SQLQuery("INSERT INTO visit VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                                     ).executeCommit((bookingNum,self.ID,selected_row_raw[1],selected_row_raw[2],"","F","F","","","0"))
+                            SQLQuery("DELETE FROM available_time WHERE StaffID = ? AND Timeslot = ?"
+                                     ).executeCommit((selected_row_raw[1], selected_row_raw[2]))
+
+                            print("booked successfully.")
+
+                            visit_result = SQLQuery("SELECT bookingNo, NHSNo, StaffID, Timeslot FROM visit "
+                                                    "WHERE bookingNo = ? "
+                                                    ).executeFetchAll(parameters=(bookingNum,))
+
+                            print(tabulate(visit_result, headers=["bookingNo","NHSNo", "GP", "Timeslot"]))
+
+                            input("Press Enter to continue...")
+                            return True
+                    except:
+                        print("Error encountered")
+                        input("Press Enter to continue...")
+                if confirm == "N":
+                    print("book cancelled.")
+                    slots_to_remove = []
+                    input("Press Enter to continue...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def reviewApmt(self):
         pass
 
-    def bookApmt(self):
+    def cancel_appointment(self):
         pass
-
-    def cancelApmt(self):
-        pass
-
-    def editApmt(self):
-        pass
-
 
 if __name__ == "__main__":
     current_user = MenuHelper.login()
